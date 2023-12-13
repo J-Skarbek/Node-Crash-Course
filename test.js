94bf57bdb123d6fe99809c6dc5b017bd108aabe2
J-Skarbek <john@snarkmediaagency.com>
2023-12-13 13:45:20 -0600

    Testing global methods

---
+++ test.js
@@ -0,0 +1,22 @@
+const myName = "John A. Skarbek";
+
+let interval = 0
+
+console.log(myName);
+
+const greet = input => console.log(`hello, ${input}`);
+
+// greet(myName);
+
+console.log(global);
+
+setTimeout(() => {
+  console.log('in the timeout');
+  clearInterval(int)
+}, 4000)
+
+
+const int = setInterval(() => {
+  console.log(interval += 1)
+}, 1000)
+
