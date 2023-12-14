ac09ddcc69d0fb866ef67758dfda63d345059b44
J-Skarbek <john@snarkmediaagency.com>
2023-12-14 09:36:21 -0600

    Importing/exporting mulitple items from modules

--- people.js
+++ people.js
@@ -1,3 +1,11 @@
 const people = ['John', 'Mike', 'Jen', 'Jorge'];
+const ages = [37, 37, 32, 10];
 
-console.log(people);
+// console.log(people);
+
+module.exports = {
+  // people: people,
+  people,
+  // ages: ages
+  ages
+}
