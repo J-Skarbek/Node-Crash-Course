641be9e3e7c60f04b98422b498256ea6a2152f98
J-Skarbek <john@snarkmediaagency.com>
2023-12-14 14:32:20 -0600

    One last save to bring everything current

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const http = require('http');
 
-
-// This method acutally creates the server, and can be stored as
-// const/let to use in advanced cases like creating web sockets
-
 // 1st argument takes a callback function, which runs anytime a request
 // comes into this server we created. This callback function provided access
 // to two objects: the request and response objects
@@ -12,7 +8,8 @@ const server = http.createServer((req, res) => {
 });
 
 // .listen() method actually allows the server to listen for requests
-// 1st argument is the port number
+// 1st argument is the port number -- 3000 is commonly used for local
+// web development
 // 2nd arugment is the hostname -- it's defaulted to 'localhost' but it's
 // explicitily called in this example
 // 3rd argument is a callback that fires whenever a request is made to the
@@ -20,4 +17,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, 'localhost', () => {
   console.log('listening for requests on port 3000')
-})
+});
