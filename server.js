13ef46073ccada62693ce6031324321a2f135fcd
J-Skarbek <john@snarkmediaagency.com>
2023-12-15 17:28:27 -0600

    Create a simple routing system

--- server.js
+++ server.js
@@ -11,8 +11,21 @@ const server = http.createServer((req, res) => {
   // set Header content type
     res.setHeader('Content-Type', 'text/html');
 
+    let path = './views/';
+    switch(req.url) {
+      case '/':
+        path += 'index.html';
+        break;
+      case '/about':
+        path += 'about.html';
+        break;
+      default:
+        path += '404.html';
+        break;
+    }
+
   // Send an HTML file
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end(); // good practice to use this whenever a request is made and needs a response to prevent hanging in the browser
